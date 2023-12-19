f6461dee5e59d1ee8a2417986cd0c92abe686f2a
assyifff <syifanurulfitri@outlook.com>
2023-12-19 13:58:28 +0700

    update content list pengajuan

--- application/views/wasa/user_korban_bencana/content_pengajuan_list.php
+++ application/views/wasa/user_korban_bencana/content_pengajuan_list.php
@@ -94,7 +94,7 @@
                 <small class="font-bold">Silakan isi identitas formulir pengajuan bantuan</small>
             </div>
             <input type="hidden" class="form-control" value="" name="FILE_NAME_TEMP" id="FILE_NAME_TEMP" disabled />
-            <!-- <input type="text" class="form-control" value="" name="CODE_MD5" id="CODE_MD5" disabled /> -->
+            <input type="hidden" class="form-control" value="" name="CODE_MD5" id="CODE_MD5" disabled />
 
             <div class="form-horizontal">
                 <div class="modal-body">
